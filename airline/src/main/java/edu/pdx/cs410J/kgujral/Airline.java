a494c2d89293df010e4c3088ffe40cfed8c8ddd0
kgujral2612 <kgujral@pdx.edu>
2023-01-23 21:54:58 -0800

    Added the logic for adding flights and improved airline test cases

--- airline/src/main/java/edu/pdx/cs410J/kgujral/Airline.java
+++ airline/src/main/java/edu/pdx/cs410J/kgujral/Airline.java
@@ -2,13 +2,16 @@ package edu.pdx.cs410J.kgujral;
 
 import edu.pdx.cs410J.AbstractAirline;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Airline extends AbstractAirline<Flight> {
   private final String name;
+  private Collection<Flight> flights;
 
   public Airline(String name) {
     this.name = name;
+    this.flights = new ArrayList<Flight>();
   }
 
   @Override
@@ -18,11 +21,11 @@ public class Airline extends AbstractAirline<Flight> {
 
   @Override
   public void addFlight(Flight flight) {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    flights.add(flight);
   }
 
   @Override
   public Collection<Flight> getFlights() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return flights;
   }
 }
--- airline/src/test/java/edu/pdx/cs410J/kgujral/AirlineTest.java
+++ airline/src/test/java/edu/pdx/cs410J/kgujral/AirlineTest.java
@@ -2,6 +2,9 @@ package edu.pdx.cs410J.kgujral;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,13 +17,23 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  */
 public class AirlineTest {
     Airline sample_airline = new Airline("Kingfisher Airlines");
-   @Test
+    Flight sample_flight = new Flight(3561, "DDN", "DDL", "05:00", "05:45");
+
+    /*
+     * Should create an airline
+     * when an empty name is passed
+     * */
+    @Test
     void airlineIsCreatedSuccessfullyWithEmptyName(){
        Airline airline = new Airline("");
        assertThat(airline, is(not(equalTo(nullValue()))));
        assertThat(airline.getName(), is(equalTo("")));
    }
 
+    /*
+     * Should create an airline
+     * when a valid name is passed
+     * */
     @Test
     void airlineIsCreatedSuccessfullyWithName(){
        var name = "Air India";
@@ -29,15 +42,49 @@ public class AirlineTest {
        assertThat(airline.getName(), is(equalTo(name)));
     }
 
+    /*
+    * Should return airline name
+    * */
     @Test
     void getNameReturnsName(){
         assertThat(sample_airline.getName(), is("Kingfisher Airlines"));
     }
 
+    /*
+    * Should add flights to the collection
+    * */
     @Test
-    void addFlightAddsFlight(){}
+    void addFlightAddsFlight(){
+       //Arrange
+       sample_airline.addFlight(sample_flight);
 
+       //Act
+       var flightCollection = sample_airline.getFlights();
+       List flights = new ArrayList(flightCollection);
+
+       //Assert
+       assertThat(flights.size(), is(equalTo(1)));
+       assertThat(flights.get(0), is(not(nullValue())));
+       assertThat(((Flight)flights.get(0)).getNumber(), is(3561));
+    }
+
+    /*
+     * Should get the updated flight collection
+     * after adding flights
+     * */
     @Test
-    void getFlightsReturnsListOfFlights(){}
+    void getFlightsReturnsListOfFlights(){
+        var airline = new Airline("My Airline");
+        var myFlight = new Flight(1234, "my src", "my dest", "my departure", "my arrival");
+        assertThat(airline.getFlights(), is(not(nullValue())));
+        assertThat(airline.getFlights().size(), is(0));
+
+        airline.addFlight(myFlight);
+        assertThat(airline.getFlights().size(), is(1));
+
+        airline.addFlight(myFlight);
+        assertThat(airline.getFlights().size(), is(2));
+
+    }
 
 }

3c940e5deaa16b1f2730f3d06c8df0cac65d0a1a
kgujral2612 <kgujral@pdx.edu>
2023-02-01 12:25:36 -0800

    renamed variable

--- airline/src/main/java/edu/pdx/cs410J/kgujral/TextParser.java
+++ airline/src/main/java/edu/pdx/cs410J/kgujral/TextParser.java
@@ -1,5 +1,4 @@
 package edu.pdx.cs410J.kgujral;
-import com.google.common.annotations.VisibleForTesting;
 import edu.pdx.cs410J.AirlineParser;
 import edu.pdx.cs410J.ParserException;
 import java.io.BufferedReader;
@@ -14,7 +13,7 @@ public class TextParser implements AirlineParser<Airline> {
     /** The reader object that reads text files */
   private final Reader reader;
     /** missing or invalid argument message */
-  static final String missingOrInvalidArgument = "Missing or Invalid %s in text file! Was %s | Expected %s";
+  static final String invalidArgument = "Invalid %s in text file! Was %s | Expected %s";
     /** io error message */
   static final String ioError = "Unable to read the given text file.";
 
@@ -35,61 +34,58 @@ public class TextParser implements AirlineParser<Airline> {
    * */
   @Override
   public Airline parse() throws ParserException {
-      boolean airlineNameNotPresent = false;
-      boolean flightInfoPresent = false;
       try (
       BufferedReader br = new BufferedReader(this.reader)) {
-          String airlineName = br.readLine();
-
-          if(!Project2.isValidAirlineName(airlineName)){
-              airlineNameNotPresent = true;
-              return null;
-          }
-
           String line;
-          int count=0;
+          int count=-1;
           int number=0;
           String src="", dest="", depart="", arrive;
 
-          var airline = new Airline(airlineName);
+          Airline airline = null;
           while ((line = br.readLine()) != null) {
             if(line.isEmpty() || line.isBlank()){
               continue;
             }
             line = line.trim();
             switch(count){
+                case -1: if(!Project2.isValidAirlineName(line)){
+                        return null;
+                    }
+                    airline = new Airline(line);
+                    count ++;
+                    break;
               case 0: if(!Project2.isValidFlightNumber(line)){
-                          throw new ParserException(String.format(missingOrInvalidArgument, "Flight Number", line, "A whole number. eg: 6478"));
+                          throw new ParserException(String.format(invalidArgument, "Flight Number", line, "A whole number. eg: 6478"));
                       }
                       number = Integer.parseInt(line);
                       count++;
                       break;
               case 1: if(!Project2.isValidAirportCode(line)){
-                          throw new ParserException(String.format(missingOrInvalidArgument, "Departure Airport Code", line, "A 3-letter String. eg: PDX"));
+                          throw new ParserException(String.format(invalidArgument, "Departure Airport Code", line, "A 3-letter String. eg: PDX"));
                       }
                       src = line;
                       count++;
                       break;
               case 2: if(!Project2.isValidDate(line.split(" ")[0])){
-                            throw new ParserException(String.format(missingOrInvalidArgument, "Departure Date", line, "A date in the format mm/dd/yyyy. eg: 11/03/1997"));
+                            throw new ParserException(String.format(invalidArgument, "Departure Date", line, "A date in the format mm/dd/yyyy. eg: 11/03/1997"));
                       }
                       if(!Project2.isValidTime(line.split(" ")[1])){
-                          throw new ParserException(String.format(missingOrInvalidArgument, "Departure Time", line, "A time in the format hh:mm. eg: 05:45"));
+                          throw new ParserException(String.format(invalidArgument, "Departure Time", line, "A time in the format hh:mm. eg: 05:45"));
                       }
                       depart = line;
                       count++;
                       break;
               case 3: if(!Project2.isValidAirportCode(line)){
-                          throw new ParserException(String.format(missingOrInvalidArgument, "Arrival Airport Code", line, "A 3-letter String. eg: PDX"));
+                          throw new ParserException(String.format(invalidArgument, "Arrival Airport Code", line, "A 3-letter String. eg: PDX"));
                       }
                       dest = line;
                       count++;
                       break;
               case 4: if(!Project2.isValidDate(line.split(" ")[0])){
-                          throw new ParserException(String.format(missingOrInvalidArgument, "Arrival Date", line, "A date in the format mm/dd/yyyy. eg: 11/03/1997"));
+                          throw new ParserException(String.format(invalidArgument, "Arrival Date", line, "A date in the format mm/dd/yyyy. eg: 11/03/1997"));
                       }
                       if(!Project2.isValidTime(line.split(" ")[1])){
-                          throw new ParserException(String.format(missingOrInvalidArgument, "Arrival Time", line, "A time in the format hh:mm. eg: 05:45"));
+                          throw new ParserException(String.format(invalidArgument, "Arrival Time", line, "A time in the format hh:mm. eg: 05:45"));
                       }
                       arrive = line;
                       airline.addFlight(new Flight(number, src, dest, depart, arrive));

a0f6f20769ac670490f51c4b869ed71c8f0090a5
kgujral2612 <kgujral@pdx.edu>
2023-03-17 18:23:23 -0700

    Added a SearchView and a ListView

--- airline-android/app/src/main/java/edu/pdx/cs410J/kgujral/MainActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410J/kgujral/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.pdx.cs410J.kgujral;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
@@ -8,12 +9,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton addBtn;
+    private SearchView searchView;
+    private ListView listView;
+
+    String[] nameList = {"KG", "Harry", "Sheena", "Shanshu", "Fury"};
+
+    ArrayAdapter<String> arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +40,41 @@ public class MainActivity extends AppCompatActivity {
         // Add intent to the button
         addBtn = findViewById(R.id.add_button);
         addBtn.setOnClickListener(view -> openAddAirline());
+
+        // Search View
+        searchView = findViewById(R.id.search_bar);
+
+        // List View
+        listView = findViewById(R.id.list_items);
+
+        initList();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                MainActivity.this.arrayAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                MainActivity.this.arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
+
+    private void initList() {
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, nameList);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "You clicked" + adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 
     private void changeStatusBarColor(){
         Window window = getWindow();
